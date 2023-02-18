a304a0f71a5c49a45514170fbf73b2681abdbc8d
Roman Hryn <grinromchik@gmail.com>
2023-02-18 15:46:35 +0200

    add: task 6

---
+++ 06_instagram_giveaway/README.md
@@ -0,0 +1,17 @@
+#  INSTAGRAM GIVEAWAY
+
+> To run the app you just need tu run ```node app``` command in your terminal
+
+Then you will get infomation about 3 needs of this program:
+
+1) In 1st task, we need to count how many unique users exist within these 20 files. For that, I have used a hash set table that gives me an opportunity to count all the records collecting them in 1 hash set of names. Then I just displayed the count of elements within the hash set.
+To make the program more effective I used async reading of every file. We don't care about the arrangement in wich the program will work for every file
+
+2) In 2nd task, we need to count the number of records that exist in *every* file of our list of files. For that, I have created an _etalon_ hash set using 1st file. I made it by sync file reader method to be sure that we already have the start point of our _etalon_ hash set to update it then while running async file readers for the other 19 files. As in 1st task, we musn't care about in which order we will update the _etalon_. Then we need to loop the other 19 files. We can do it in an async way cause that the _etalon_ storage will be global for every file reading operation. We don't care which order we will work with in every file.
+
+3) In 3rd task we need to count records that occur in at least 10 files. For that, I created a ```uniqueNamesAppearances``` object that will treat as the analog of the hash set. Every key in  ```uniqueNamesAppearances``` will be recorded as every record in the file and the value will be the number of occurrences in every file. As ent entry point ```uniqueNamesAppearances`` is empty. Then we start the loop to read every file with the next behavior while we read every file:
+ - the record does not exist within the ```uniqueNamesAppearances``` (as key) => then we will add such property in ```uniqueNamesAppearances``` with number of occurrences value equal to *1*
+ - If the record exists within the ```uniqueNames``` (as key) => then we will increase the number of occurrences of such value by it key prop within ```uniqueNames```
+
+ As the endpoint, we will have an object that has the list of records with the number of occurrences within all 20 files stored with information on how many times this record was encountered in 20 files.
+ Then we will just count how many records have encountered value more or equal to 10. It will be the answer.
---
+++ 06_instagram_giveaway/app.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const fsp = fs.promises;
+
+const fileNames = [...new Array(20)].map(
+   (el, index) => (el = `./store/out${index}.txt`)
+);
+
+function uniqueValues() {
+   let uniqueValues = new Set();
+   const start = new Date();
+
+   let amountOfUniqueRecords;
+
+   Promise.all(
+      fileNames.map(async (fileName) => {
+         let fileContent = await fsp.readFile(fileName, { encoding: 'utf8' });
+
+         uniqueValues = new Set([...uniqueValues, ...fileContent.split('\n')]);
+
+         amountOfUniqueRecords = uniqueValues.size;
+      })
+   ).then(() => {
+      console.log(
+         'Time that was spent for 1st task: ',
+         (new Date() - start) / 1000
+      );
+      console.log('Unique usernames: ', amountOfUniqueRecords);
+   });
+}
+
+function existInAllFiles() {
+   let etalon, etalonSize;
+   const start = new Date();
+
+   function intersectionCreator(setA, setB) {
+      const result = new Set();
+
+      for (const elem of setA) {
+         if (setB.has(elem)) {
+            result.add(elem);
+         }
+      }
+
+      return result;
+   }
+
+   try {
+      const data = fs.readFileSync('./store/out0.txt', 'utf8');
+      etalon = new Set(data.split('\n'));
+   } catch (err) {
+      console.error(err);
+   }
+
+   Promise.all(
+      fileNames.map(async (fileName) => {
+         let fileContent = await fs.readFileSync(fileName, {
+            encoding: 'utf8',
+         });
+         fileContent = new Set(fileContent.split('\n'));
+
+         if (fileName === 'out0.txt') {
+            return;
+         } else {
+            etalon = intersectionCreator(etalon, fileContent);
+         }
+
+         etalonSize = etalon.size;
+         return;
+      })
+   ).then(() => {
+      console.log(
+         'Time that was spent for 2st task: ',
+         (new Date() - start) / 1000
+      );
+      console.log('The number of usernames that are in each file', etalonSize);
+   });
+}
+
+function existInAtleastTen() {
+   let uniqueNamesAppearances = {};
+   const start = new Date();
+
+   Promise.all(
+      fileNames.map(async (fileName) => {
+         let fileContent = await fsp.readFile(fileName, { encoding: 'utf8' });
+
+         fileContent = new Set(fileContent.split('\n'));
+
+         fileContent.forEach((el) => {
+            if (!uniqueNamesAppearances[el]) {
+               uniqueNamesAppearances[el] = 1;
+            } else if (uniqueNamesAppearances[el] >= 0) {
+               uniqueNamesAppearances[el] += 1;
+            }
+         });
+      })
+   ).then(() => {
+      let moreThen10Counter = 0;
+
+      for (const key in uniqueNamesAppearances) {
+         if (uniqueNamesAppearances[key] >= 10) moreThen10Counter++;
+      }
+      console.log(
+         'Number of records that exist in at least 10 files',
+         moreThen10Counter
+      );
+      console.log(
+         'Time that was spent for 3st task: ',
+         (new Date() - start) / 1000
+      );
+   });
+}
+
+uniqueValues();
+existInAllFiles();
+existInAtleastTen();
